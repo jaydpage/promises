2dbd2ce5e28e79a187b237480a145090786e8d42
Jayd Page <jaydpage@gmail.com>
2018-12-16 11:05:57 +0200

    add tests around debounce

--- src/Debounce.test.ts
+++ src/Debounce.test.ts
@@ -4,7 +4,7 @@ describe("Debounce", () => {
   jest.useFakeTimers();
 
   describe("doAfterDelay", () => {
-    it("should execute the provided callback after 500ms", () => {
+    it("should execute the provided callback after the timeout period", () => {
       // Arrange
       const debounce = new Debounce();
       const callback = jest.fn();
@@ -12,7 +12,26 @@ describe("Debounce", () => {
       debounce.doAfterDelay(callback);
       jest.runOnlyPendingTimers();
       // Assert
-      expect(callback).toHaveBeenCalled();
+      expect(callback).toHaveBeenCalledTimes(1);
+    });
+    it("should execute only the last provided callback after the timeout period", () => {
+      // Arrange
+      const debounce = new Debounce();
+      const callback1 = jest.fn();
+      const callback2 = jest.fn();
+      const callback3 = jest.fn();
+      // Act
+      debounce.doAfterDelay(callback1);
+      jest.advanceTimersByTime(400);
+      debounce.doAfterDelay(callback2);
+      jest.advanceTimersByTime(450);
+      debounce.doAfterDelay(callback3);
+      jest.runAllTimers();
+
+      // Assert
+      expect(callback1).not.toHaveBeenCalled();
+      expect(callback2).not.toHaveBeenCalled();
+      expect(callback3).toHaveBeenCalled();
     });
   });
 });
